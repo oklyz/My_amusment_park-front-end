05df66ebbcd5640b4ddced2b2c7d56cf43424b65
Manaf-10 <manaf.alhujairi2006@gmail.com>
2025-08-04 14:59:46 +0300

    files structure  and depnedancies

---
+++ My_amusment_park(front-end)/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <Home />
+    </>
+  );
+}
+
+export default App;
